36c71f07589428fa2a2d6ea610e1ae19c44c6e42
talamus1972 <talamus1972@gmail.com>
2023-10-14 19:11:19 +0300

    rr

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,14 @@
 function calculateTotal(number) {
-  let cat = 0;
-while (let dog = 1; dog <= number; dog += 1) {
-  cat += 1;
-   
-}
- return cat;
+  let sum = 0;
+  for (let i = 1; i <= number; i += 1) {
+    sum += i;
+  }
+  return sum;
 }
 
 calculateTotal(1); //повертає 1
 console.log(calculateTotal(3)); //повертає 6
 calculateTotal(0); //повертає 0
-calculateTotal(18); //повертає 171
+console.log(calculateTotal(18)); //повертає 171
 calculateTotal(24); //повертає 300
 calculateTotal();
