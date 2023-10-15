517da58d9334a7170cd924a80dbec330a53b237f
talamus1972 <talamus1972@gmail.com>
2023-10-16 00:34:50 +0300

    eeee

---
+++ js/task-6.js
@@ -0,0 +1,27 @@
+function createArrayOfNumbers(min, max) {
+for (let i = min; i < max; i++){
+        if (array[i] === value) {
+            return i;
+        }     
+               
+           
+    }        
+   
+}
+
+
+
+
+
+
+
+
+
+console.log(createArrayOfNumbers(1, 3));
+//повертає [1, 2, 3]
+console.log(createArrayOfNumbers(14, 17));
+//повертає [14, 15, 16, 17]
+console.log(createArrayOfNumbers(29, 34));
+//повертає [29, 30, 31, 32, 33, 34]
+console.log(createArrayOfNumbers());
+//з випадковими min і max повертає правильний масив
