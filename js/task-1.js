8cc96ff10e3ac9669a17e0e8bb790e0774d70078
talamus1972 <talamus1972@gmail.com>
2023-10-22 15:10:56 +0300

    222

--- js/task-1.js
+++ js/task-1.js
@@ -6,9 +6,34 @@
 //   return sum;
 // }
 
-calculateTotal(1); //повертає 1
-console.log(calculateTotal(3)); //повертає 6
-calculateTotal(0); //повертає 0
-console.log(calculateTotal(18)); //повертає 171
-calculateTotal(24); //повертає 300
-calculateTotal();
+// calculateTotal(1); //повертає 1
+// console.log(calculateTotal(3)); //повертає 6
+// calculateTotal(0); //повертає 0
+// console.log(calculateTotal(18)); //повертає 171
+// calculateTotal(24); //повертає 300
+// calculateTotal();
+
+// function calculateTotalPrice(order) {
+//   let sum = 0;
+//   for (let i = 0; i < order.length; i += 1) {
+//     sum += order[i];
+//   }
+//   return sum;
+// }
+
+// console.log(calculateTotalPrice([12, 85, 37, 4])); //повертає 138
+// calculateTotalPrice([164, 48, 291]); //повертає 503
+// calculateTotalPrice([412, 371, 94, 63, 176]); //повертає 1116
+
+function getEvenNumbers(start, end) {
+  for (let i = start; i < end.length; i++) {
+    if (i % 2 === 0) {
+      console.log(i);
+      //   return [index];
+      // } else {
+      //   return [];
+      // }
+    }
+  }
+}
+// console.log(getEvenNumbers(2, 5)); //[2, 4]
--- js/task-6.js
+++ js/task-6.js
@@ -1,27 +1,14 @@
 function createArrayOfNumbers(min, max) {
-for (let i = min; i < max; i++){
-        if (array[i] === value) {
-            return i;
-        }     
-               
-           
-    }        
-   
+  let i = min;
+  for (; i <= max; i++) {
+    break;
+  }
+  console.log([i]);
 }
 
-
-
-
-
-
-
-
-
 console.log(createArrayOfNumbers(1, 3));
-//повертає [1, 2, 3]
-console.log(createArrayOfNumbers(14, 17));
-//повертає [14, 15, 16, 17]
-console.log(createArrayOfNumbers(29, 34));
-//повертає [29, 30, 31, 32, 33, 34]
-console.log(createArrayOfNumbers());
-//з випадковими min і max повертає правильний масив
+// //повертає [1, 2, 3]
+// console.log(createArrayOfNumbers(14, 17));
+// //повертає [14, 15, 16, 17]
+// console.log(createArrayOfNumbers(29, 34));
+// //повертає [29, 30, 31, 32, 33, 34]
