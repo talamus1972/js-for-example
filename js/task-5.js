c2c928a25c9d58c116cbe559414cf719cae3f4a5
talamus1972 <talamus1972@gmail.com>
2023-10-29 23:43:56 +0200

    2

--- js/task-5.js
+++ js/task-5.js
@@ -1,23 +1,40 @@
-function getSlice(array, value) {
-    for (let i = 0; i < array.length; i++){
-        if (array[i] === value) {
-            return array.slice(0, i + 1);
-        }     
+// function getSlice(array, value) {
+//     for (let i = 0; i < array.length; i++){
+//         if (array[i] === value) {
+//             return array.slice(0, i + 1);
+//         }
                
            
-    }        
-   return array.slice(0, 0);
-}
+//     }
+//    return array.slice(0, 0);
+// }
 
 
 
-console.log(getSlice(["Mango", "Poly", "Ajax"], "Poly"));
-//повертає ["Mango", "Poly"]
-console.log(getSlice(["Mango", "Poly", "Ajax"], "Ajax"));
-//повертає ["Mango", "Poly", "Ajax"]
-console.log(getSlice(["Mango", "Poly", "Ajax"], "Mango"));
-//повертає ["Mango"]
-console.log(getSlice(["Mango", "Poly", "Ajax"], "Jacob"));
-//повертає []
-console.log(getSlice(["Mango", "Poly", "Ajax"], "Casey"));
-//повертає []
+// console.log(getSlice(["Mango", "Poly", "Ajax"], "Poly"));
+// //повертає ["Mango", "Poly"]
+// console.log(getSlice(["Mango", "Poly", "Ajax"], "Ajax"));
+// //повертає ["Mango", "Poly", "Ajax"]
+// console.log(getSlice(["Mango", "Poly", "Ajax"], "Mango"));
+// //повертає ["Mango"]
+// console.log(getSlice(["Mango", "Poly", "Ajax"], "Jacob"));
+// //повертає []
+// console.log(getSlice(["Mango", "Poly", "Ajax"], "Casey"));
+// //повертає []
+
+
+function calculateTax(amount, taxRate = 0.2) {
+
+    const apple = amount * taxRate;
+    return apple;
+ }
+
+
+ console.log(calculateTax(100, 0.1)); //повертає 10
+ console.log(calculateTax(200, 0.1)); //повертає 20
+ console.log(calculateTax(100, 0.2)); //повертає 20
+ console.log(calculateTax(200, 0.2)); //повертає 40
+ console.log(calculateTax(100, 0.3)); //повертає 30
+ console.log(calculateTax(200, 0.3)); //повертає 60
+ console.log(calculateTax(100)); //повертає 20
+ console.log(calculateTax(200)); //повертає 40
--- js/task-6.js
+++ js/task-6.js
@@ -1,30 +1,105 @@
 // function createArrayOfNumbers(min, max) {
-//   let i = min;
-//   for (; i <= max; i++) {
-//     break;
+//     const result = [];
+//     for (let i = min; i <= max; i++) {
+//         result.push(i);
+//     }
+//     return result;
+// }
+// console.log(createArrayOfNumbers(1, 3));
+// // // //повертає [1, 2, 3]
+// console.log(createArrayOfNumbers(14, 17));
+// // // //повертає [14, 15, 16, 17]
+// console.log(createArrayOfNumbers(29, 34));
+// // // //повертає [29, 30, 31, 32, 33, 34]
+
+
+
+// function checkStorage(storage, item) {
+//   const items = item.toLowerCase();
+//   // console.log(items);
+//   if (storage.includes(items)) {
+//     return `${items} is available to order!`;
+//   } else {
+//     return `Sorry! We are out of stock!`;
 //   }
-//   console.log([i]);
 // }
 
-// console.log(createArrayOfNumbers(1, 3));
-// // //повертає [1, 2, 3]
-// // console.log(createArrayOfNumbers(14, 17));
-// // //повертає [14, 15, 16, 17]
-// // console.log(createArrayOfNumbers(29, 34));
-// // //повертає [29, 30, 31, 32, 33, 34]
-function checkStorage(storage, item) {
-  const items = item.toLowerCase();
-  // console.log(items);
-  if (storage.includes(items)) {
-    return `${items} is available to order!`;
-  } else {
-    return `Sorry! We are out of stock!`;
+// console.log(checkStorage(['apple', 'plum', 'pear'], 'plum')); //повертає "plum is available to order!"
+// console.log(checkStorage(['apple', 'plum', 'pear'], 'pLuM')); //повертає "plum is available to order!"
+// console.log(checkStorage(['apple', 'plum', 'pear'], 'pear')); //повертає "pear is available to order!"
+// console.log(checkStorage(['apple', 'plum', 'pear'], 'pEAr')); //повертає "pear is available to order!"
+// console.log(checkStorage(['apple', 'plum', 'pear'], 'orange')); //повертає "Sorry! We are out of stock!"
+// console.log(checkStorage(['apple', 'plum', 'pear'], 'carrot')); //повертає "Sorry! We are out of stock!"
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4.7,
+//   price: 5000,
+//   tags: ["premium", "promoted", "top", "trusted"],
+//   owner: {
+//     name: "Henry Sibola",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// apartment.area = 60;
+// apartment.rooms = 3;
+// apartment.location = {};
+// apartment.location.country = "Jamaica";
+// apartment.location.city = "Kingston";
+
+
+
+// console.log(apartment);
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+
+// const keys = [];
+// const values = [];
+
+// for (let key in apartment) {
+//     keys.push(key);
+//     values.push(apartment[key]);
+// }
+
+// console.log(keys); // logs ["descr", "rating", "price"]
+// console.log(values); // logs ["Spacious apartment in the city center", 4, 2153]
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// const keys = Object.keys(apartment);
+
+// for (const key of keys) {
+//   values.push(apartment[key]);
+// }
+
+// console.log(keys);
+// console.log(values);
+
+function countProps(object) {
+
+
+    // const keys = Object.keys(object);
+
+
+  for (let i = 0; i < object.length; i++) {
+   
+    console.log(i.length);
   }
+  
+  
 }
 
-console.log(checkStorage(['apple', 'plum', 'pear'], 'plum')); //повертає "plum is available to order!"
-console.log(checkStorage(['apple', 'plum', 'pear'], 'pLuM')); //повертає "plum is available to order!"
-console.log(checkStorage(['apple', 'plum', 'pear'], 'pear')); //повертає "pear is available to order!"
-console.log(checkStorage(['apple', 'plum', 'pear'], 'pEAr')); //повертає "pear is available to order!"
-console.log(checkStorage(['apple', 'plum', 'pear'], 'orange')); //повертає "Sorry! We are out of stock!"
-console.log(checkStorage(['apple', 'plum', 'pear'], 'carrot')); //повертає "Sorry! We are out of stock!"
+console.log(countProps({})); //повертає 0
+console.log(countProps({ name: "Mango", age: 2 })); //повертає 2
+console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 })); ///повертає 3
