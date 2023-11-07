a9b3f20fcc1b1e57134835e5f01453e38bff4812
talamus1972 <talamus1972@gmail.com>
2023-11-07 18:12:24 +0200

    ee

--- js/task-10.js
+++ js/task-10.js
@@ -2,7 +2,6 @@
 //   return users.map(user => user.email);
 // };
 
-
 // ----------------------------------------------------
 
 // const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
@@ -13,75 +12,86 @@
 // console.log(oddNumbers);
 
 // ------------------------------------------------------
-const color = "blue";
+
 const users = [
   {
-    name: "Moore Hensley",
-    email: "moorehensley@indexia.com",
-    eyeColor: "blue",
-    friends: ["Sharron Pace"],
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
     isActive: false,
     balance: 2811,
-    gender: "male"
+    gender: 'male',
+    age: 37,
   },
   {
-    name: "Sharlene Bush",
-    email: "sharlenebush@tubesys.com",
-    eyeColor: "blue",
-    friends: ["Briana Decker", "Sharron Pace"],
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
     isActive: true,
     balance: 3821,
-    gender: "female"
+    gender: 'female',
+    age: 34,
   },
   {
-    name: "Ross Vazquez",
-    email: "rossvazquez@xinware.com",
-    eyeColor: "green",
-    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
     isActive: false,
     balance: 3793,
-    gender: "male"
+    gender: 'male',
+    age: 24,
   },
   {
-    name: "Elma Head",
-    email: "elmahead@omatom.com",
-    eyeColor: "green",
-    friends: ["Goldie Gentry", "Aisha Tran"],
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
     isActive: true,
     balance: 2278,
-    gender: "female"
+    gender: 'female',
+    age: 21,
   },
   {
-    name: "Carey Barr",
-    email: "careybarr@nurali.com",
-    eyeColor: "blue",
-    friends: ["Jordan Sampson", "Eddie Strong"],
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
     isActive: true,
     balance: 3951,
-    gender: "male"
+    gender: 'male',
+    age: 27,
   },
   {
-    name: "Blackburn Dotson",
-    email: "blackburndotson@furnigeer.com",
-    eyeColor: "brown",
-    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
     isActive: false,
     balance: 1498,
-    gender: "male"
+    gender: 'male',
+    age: 38,
   },
   {
-    name: "Sheree Anthony",
-    email: "shereeanthony@kog.com",
-    eyeColor: "brown",
-    friends: ["Goldie Gentry", "Briana Decker"],
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
     isActive: true,
     balance: 2764,
-    gender: "female"
-  }
-]
+    gender: 'female',
+    age: 39,
+  },
+];
 
-const getUsersWithEyeColor = (users, color) => {
-    return users.filter(user => user.eyeColor === color).map(user => user.name);
-};
+// const getUsersWithEyeColor = (users, color) =>
+//   users.filter(user => user.eyeColor === color);
 
-console.log(getUsersWithEyeColor(users, color));
+// console.log(getUsersWithEyeColor(users, 'blue'));
+
+const getUsersWithAge = (users, minAge, maxAge) =>
+  users.filter(user => user.age > minAge && user.age < maxAge);
+
+console.log(getUsersWithAge(users, 20, 30));
