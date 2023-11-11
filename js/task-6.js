bf87da000b0b79b6e676d5c38e046726172c7cb2
talamus1972 <talamus1972@gmail.com>
2023-11-11 19:05:30 +0200

    ss

--- js/task-6.js
+++ js/task-6.js
@@ -1,30 +1,38 @@
-const pizzaPalace = {
-  pizzas: ['Supercheese', 'Smoked', 'Four meats'],
-  checkPizza(pizzaName) {
-    return pizzas.includes(pizzaName);
-  },
-  order(pizzaName) {
-    const isPizzaAvailable = checkPizza(pizzaName);
+// const pizzaPalace = {
+//   pizzas: ['Supercheese', 'Smoked', 'Four meats'],
+//   checkPizza(pizzaName) {
+//     return this.pizzas.includes(pizzaName);
+//   },
+//   order(pizzaName) {
+//     const isPizzaAvailable = this.checkPizza(pizzaName);
 
-    if (!isPizzaAvailable) {
-      return `Sorry, there is no pizza named «${pizzaName}»`;
-    }
+//     if (!isPizzaAvailable) {
+//       return `Sorry, there is no pizza named «${pizzaName}»`;
+//     }
 
-    return `Order accepted, preparing «${pizzaName}» pizza`;
-  },
-};
+//     return `Order accepted, preparing «${pizzaName}» pizza`;
+//   },
+// };
 
-pizzaPalace.order('Smoked'); //повертає рядок "Order accepted, preparing «Smoked» pizza"
+// pizzaPalace.order('Smoked'); //повертає рядок "Order accepted, preparing «Smoked» pizza"
+// console.log("🚀  pizzaPalace.order('Smoked');", pizzaPalace.order('Smoked'));
 
-pizzaPalace.order('Four meats'); //повертає рядок "Order accepted, preparing «Four meats» pizza"
+// pizzaPalace.order('Four meats'); //повертає рядок "Order accepted, preparing «Four meats» pizza"
+// console.log(
+//   "🚀  pizzaPalace.order('Four meats');",
+//   pizzaPalace.order('Four meats')
+// );
 
-console.log("🚀 ~ pizzaPalace;", pizzaPalace)
+// pizzaPalace.order('Big Mike'); //повертає рядок "Sorry, there is no pizza named «Big Mike»"
+// console.log(
+//   "🚀  pizzaPalace.order('Big Mike');",
+//   pizzaPalace.order('Big Mike')
+// );
 
+// pizzaPalace.order('Viennese'); //повертає рядок "Sorry, there is no pizza named «Viennese»"
+// console.log(
+//   "🚀  pizzaPalace.order('Viennese');",
+//   pizzaPalace.order('Viennese')
+// );
 
-console.log("🚀 ~ pizzaPalace;", pizzaPalace)
-
-
-console.log("🚀 ~ pizzaPalace;", pizzaPalace)
-
-pizzaPalace.order('Big Mike'); //повертає рядок "Sorry, there is no pizza named «Big Mike»"
-pizzaPalace.order('Viennese'); //повертає рядок "Sorry, there is no pizza named «Viennese»"
+// ===============================================================================================
